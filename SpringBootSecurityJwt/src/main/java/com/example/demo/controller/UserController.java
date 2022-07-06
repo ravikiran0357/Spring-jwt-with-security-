5a5883262ea32c0bcb68ac4e338df487cd1e5336
sid <sid@sparity-ThinkPad-E490>
2022-07-06 16:40:31 +0530

    jwt new

--- SpringBootSecurityJwt/src/main/java/com/example/demo/controller/UserController.java
+++ SpringBootSecurityJwt/src/main/java/com/example/demo/controller/UserController.java
@@ -24,7 +24,7 @@ public class UserController {
 	private UserService service;
 	@Autowired
 	private JwtUtil util;
-	
+	//changed
 	@Autowired
 	private AuthenticationManager authenticationManager;
 
